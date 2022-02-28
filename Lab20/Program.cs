47abcffc4da6cba9c996e9265b7e31d5496b3981
Михаил <stulstoldevyat@gmail.com>
2022-03-01 00:00:23 +0300

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab20
+{
+    class Program
+    {
+        delegate double LogDelegate(double num);
+        static void Main(string[] args)
+        {
+            LogDelegate logDelegate = Task.Length;            
+            logDelegate += Task.Square;
+            logDelegate += Task.Volume;
+            logDelegate(2.5);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab20/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab20
+{
+    class Task
+    {
+        static public double Length(double r) 
+        {
+            double l = Math.PI * 2 * r;
+            Console.WriteLine("{0:f3}", l);
+            return l;
+        }
+        static public double Square(double r)
+        {
+            double s = Math.PI * r * r;
+            Console.WriteLine("{0:f3}",s);
+            return s;
+        }
+
+        static public double Volume(double r)
+        {
+            double v = 4 / 3 * Math.PI * 4 * Math.Pow(r, 3);
+            Console.WriteLine("{0:f3}", v);
+            return v;
+        }
+        
+
+    }
+}
